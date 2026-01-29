2435940e9ba83a33c012b6eecbe3727c7515a2c6
Khush-Shah12 <kushah2412@gmail.com>
2026-01-29 23:41:44 +0530

    Initial Commit

---
+++ Assignment1/Mathematical_operations.py
@@ -0,0 +1,16 @@
+# program for Mathematical operations
+
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+#operations
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
---
+++ README.md
@@ -0,0 +1,1 @@
+# Repo for tutedude Python Assignments
